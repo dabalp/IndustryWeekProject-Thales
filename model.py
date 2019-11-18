96d3e5dedc452e1d0671ff23e78202065b1cb6cb
Mihai <mihai.anca13@gmail.com>
2019-11-18 19:37:11 +0000

    Calculate flatten layer size

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
 BATCH_SIZE = 100
 EPOCHS = 3
 NUM_CLASSES = 10
 IMAGE_SIZE = (28, 28)
+
 
 class TorchModel(nn.Module):
     def __init__(self):
@@ -19,7 +19,7 @@ class TorchModel(nn.Module):
         self.conv2 = nn.Conv2d(30, 15, 3)
         self.pool2 = nn.MaxPool2d(2, 2)
         self.dropout1 = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(30976, 128)
+        self.fc1 = nn.Linear(375, 128)
         self.fc2 = nn.Linear(128, 50)
         self.fc3 = nn.Linear(50, NUM_CLASSES)
 
@@ -35,15 +35,15 @@ class TorchModel(nn.Module):
         self.loss_function = nn.NLLLoss()
         self.optimizer = optim.Adam(self.parameters(), lr=0.001)
 
-
     def forward(self, x):
+        x = x.to(self.device)
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.dropout1(x)
 
-        print(x[0].shape[0]*x[0].shape[1]*x[0].shape[2])
+        print(x[0].shape[0] * x[0].shape[1] * x[0].shape[2])
 
-        x = x.view(-1, 30976)
+        x = x.view(-1, 375)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.softmax(self.fc3(x), dim=1)
@@ -64,8 +64,6 @@ class TorchModel(nn.Module):
                 loss.backward()
                 self.optimizer.step()
             print(loss)
-
-
 
 
 if __name__ == "__main__":

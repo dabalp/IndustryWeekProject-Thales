29e99437cea4851da9e5583851abbc4669e1c96a
Athanasios Martsopoulos <amartsop@gmail.com>
2019-11-19 10:38:13 +0000

    Add load data function and training set

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision
+from torchvision import transforms, datasets
 
 BATCH_SIZE = 100
 EPOCHS = 3
@@ -41,7 +43,7 @@ class TorchModel(nn.Module):
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.dropout1(x)
 
-        print(x[0].shape[0] * x[0].shape[1] * x[0].shape[2])
+        # print(x[0].shape[0] * x[0].shape[1] * x[0].shape[2])
 
         x = x.view(-1, 375)
         x = F.relu(self.fc1(x))
@@ -50,14 +52,21 @@ class TorchModel(nn.Module):
 
         return x
 
-    def batch_train(self, train_X, train_y):
+    def batch_train(self):
         for epoch in range(EPOCHS):
-            for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
-                batch_X = train_X[i:i + BATCH_SIZE].view(-1, 1, IMAGE_SIZE[0], IMAGE_SIZE[1])
-                batch_y = train_y[i:i + BATCH_SIZE]
-
+            # for i in tqdm(range(0, len(self.trainset), BATCH_SIZE)):
+            for data in self.trainset:
+            # for i in range(len(self.trainset)):
+                # print(self.trainset.size())
+                # break
+                # self.train_X, self.train_y = self.trainset[i: i+BATCH_SIZE]
+                # batch_X = self.train_X[i:i + BATCH_SIZE].view(-1, 1, IMAGE_SIZE[0], IMAGE_SIZE[1])
+                # batch_y = self.train_y[i:i + BATCH_SIZE]
+                
+                # batch_X, batch_y = self.trainset
+                batch_X, batch_y = data
                 batch_X, batch_y = batch_X.to(self.device), batch_y.to(self.device)
-
+                
                 self.zero_grad()
                 outputs = self(batch_X)
                 loss = self.loss_function(outputs, batch_y)
@@ -65,9 +74,25 @@ class TorchModel(nn.Module):
                 self.optimizer.step()
             print(loss)
 
+    def load_data(self):
+        train = datasets.MNIST("", train=True, download=True, 
+            transform=transforms.Compose([transforms.ToTensor()]))
+  
+        test = datasets.MNIST("", train=False, download=True, 
+            transform=transforms.Compose([transforms.ToTensor()]))
+
+        self.trainset = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, 
+            shuffle=True)
+        self.testset = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, 
+            shuffle=False)
+
+
 
 if __name__ == "__main__":
     n = TorchModel()
-    a = np.random.uniform(size=IMAGE_SIZE)
-    a = torch.Tensor(a).view(-1, 1, IMAGE_SIZE[0], IMAGE_SIZE[1])
-    n.forward(a)
+    n.load_data()
+    n.batch_train()
+    torch.save(n.state_dict(), "params.pt")
+    # a = np.random.uniform(size=IMAGE_SIZE)
+    # a = torch.Tensor(a).view(-1, 1, IMAGE_SIZE[0], IMAGE_SIZE[1])
+    # n.forward(a)
